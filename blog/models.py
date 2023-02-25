7a29e5fca9ace0c0ec48d2ceaa46fc2cacba896d
seafish <seafish5252@gmail.com>
2023-02-25 22:34:33 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 class Post(models.Model):
@@ -10,7 +11,8 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return f'[{self.pk}]{self.title}'
 
